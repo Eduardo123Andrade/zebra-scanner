f1b6a4cc7064f2fd32a800f18a85093bc101eea0
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2024-02-13 20:27:23 -0300

    Removed useless code

--- App.tsx
+++ App.tsx
@@ -1,25 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react'
 
-import React from 'react';
-import {StyleSheet} from 'react-native';
-
-import {Navigator} from './navigators';
+import {Navigator} from './navigators'
 
 function App(): React.JSX.Element {
-  // const scannerResponse = useScanner(console.log);
-
-  return <Navigator />;
+  return <Navigator />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
